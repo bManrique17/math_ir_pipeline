f26730d2fdcaf17ea9c1dcb155d332932722993d
Bryan Amador <bmanrique17@gmail.com>
2026-07-03 21:33:21 -0400

    added postgres connection

--- ETL/main.py
+++ ETL/main.py
@@ -1,10 +1,15 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from sqlalchemy import create_engine, text
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
+
+    engine = create_engine(cfg.db.postgres_connection_string)
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
 
 
 if __name__ == "__main__":
